ebde771f858f72ad12b0b361ac35e42af7b1bc00
Megan Christine McClure <mcclme02@luther.edu>
2020-03-31 00:17:09 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,19 +1,19 @@
-class Person 
+# class Person 
   
-  def name=(persons_name)
-    @name = persons_name
-  end 
+#   def name=(persons_name)
+#     @name = persons_name
+#   end 
   
-  def name
-    @name 
-  end
+#   def name
+#     @name 
+#   end
   
-  def job=(persons_job)
-    @job = persons_job
-  end 
+#   def job=(persons_job)
+#     @job = persons_job
+#   end 
   
-  def job
-    @job 
-  end 
+#   def job
+#     @job 
+#   end 
   
-end 
+# end 
